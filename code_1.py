ed3b3fea16243b188eb850b952abe3f9737f3f0e
healthylife1191 <159755852+healthylife1191@users.noreply.github.com>
2024-05-21 12:22:54 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,40 @@
+
+    for item in data:
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+
+
+def generate_random_data():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    main()    data = generate_random_data()
+    return data
+
+
+    for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+def main():
+import random
+    data = generate_random_data()
+    for item in data:
+def main():
+
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():
+    return data
